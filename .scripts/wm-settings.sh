c2d62f0f5db81eddbf8dbaecf876184a1052cc28
djpalumbo <davejpalumbo@gmail.com>
2021-10-03 16:40:27 -0700

    Improve HDMI script and functionality

--- .scripts/wm-settings.sh
+++ .scripts/wm-settings.sh
@@ -3,9 +3,14 @@
 ##############
 # Window transparency
 picom -b
+# Ensure that when HDMI output is enabled it is above the laptop display
+# TODO: Update this to use variables in ~/.scripts/toggle-hdmi-out
+if xrandr | grep -E "HDMI-1 connected [[:digit:]]+x[[:digit:]]+"; then
+  xrandr --output HDMI-1 --auto --above eDP-1
+fi
 # Set desktop wallpaper and terminal colorscheme (set via ~/.wallpaper).
 # Also, note the (cat ~/.cache/wal/sequences &) in .bashrc & .zshrc
-wal -i ~/Pictures/Wallpapers -o ~/.scripts/wal-set -e
+wal -i ~/Pictures/Wallpapers -o ~/.scripts/wal-set
 
 ####################
 # STARTUP SETTINGS #
