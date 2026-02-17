a21c0eb80435775f37d679778021446799ed0fb1
djpalumbo <davejpalumbo@gmail.com>
2026-02-17 12:37:54 -0800

    wal theme rofi, dunst; fix nvim theme refresh

--- .config/nvim/lua/user/plugin_setup/pywal.lua
+++ .config/nvim/lua/user/plugin_setup/pywal.lua
@@ -4,3 +4,13 @@ if not status_ok then
 end
 
 pywal.setup()
+
+-- Reload colorscheme whenever wal regenerates colors
+local w = (vim.uv or vim.loop).new_fs_event()
+w:start(vim.fn.expand("~/.cache/wal"), {}, function(err, filename)
+  if not err and filename == "colors-wal.vim" then
+    vim.schedule(function()
+      pywal.setup()
+    end)
+  end
+end)
